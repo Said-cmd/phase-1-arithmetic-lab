b2414af1692a09e9b93b5244404c86913e87d02e
Said Hussein <saidhussein12342@gmail.com>
2022-06-20 19:17:09 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+/* Multiply */
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+/* Random */
+let random = Math.floor(Math.random()) +1;
+
+/* Mod */
+let num3 = 20;
+let num4 = 5;
+let mod = num3 / num4;
+
+/* Max */
+let max = Math.max(4,8,12,16,20);
+
